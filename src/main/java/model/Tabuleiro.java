f2afe2345c0427a2a15a395210f5c571b8959edc
Julia <juliacostapereira.jgcp@gmail.com>
2026-04-29 16:21:18 -0300

    estrutura inicial

---
+++ src/main/java/model/Tabuleiro.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Tabuleiro {
+    int[][] tabuleiro;
+
+    public Tabuleiro(){
+        this.tabuleiro=new int[9][9];
+    }
+
+}
---
+++ src/main/java/service/GeradorSudoku.java
@@ -0,0 +1,10 @@
+package service;
+
+import model.Tabuleiro;
+
+public class GeradorSudoku {
+    public Tabuleiro gerar(){
+        Tabuleiro grid = new Tabuleiro();
+        return grid;
+    }
+}
